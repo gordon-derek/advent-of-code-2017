9841fd7ba19e942658464c29dc3770886a0ea34e
gordondr196 <gordon.derek46@gmail.com>
2017-12-10 17:38:43 -0500

    Finished Day3 Part 2 Scala

--- Day3/matrixbuilder2.scala
+++ Day3/matrixbuilder2.scala
@@ -2,49 +2,79 @@ import scala.io.Source
 import scala.util.control.Breaks._
 
 
-var TARG = 361527
-val arrsz = 604
+val TARG = 361527
+var arrsz = 11
 val matrix = Array.ofDim[Int](arrsz,arrsz)
+ArrInit()
 BuildMatrix()
 //exit point always arrsz/2
 var exit = arrsz/2
 var targRow = 0
 var targCol = 0
-var target = 361527
-FindIndex()
+var target = 5       //361527
+var curNum = 1
 
-//count number of moves
-var count = 0
+PrintMat()
 
-//travel north/south first
-if(targRow > exit){
-	while(targRow != exit){
-		targRow -= 1
-		count += 1
-	}
-}
-else if(targRow < exit){
-	while(targRow != exit){
-		targRow += 1
-		count += 1
-	}
-}
-//travel east/west
-if(targCol > exit){
-	while(targCol != exit){
-		targCol -= 1
-		count += 1
-	}
-}
-else if(targCol < exit){
-	while(targCol != exit){
-		targCol += 1
-		count += 1
-	}
-}
 
-println(count)
+def GetSum(centerRow: Int, centerCol: Int): Int ={
+	var sum = 0
+	if(centerRow >= 1){
+		if(centerRow < arrsz-1){
+			if(centerCol >= 1){
+				if(centerCol < arrsz-1){
+					sum += matrix(centerRow-1)(centerCol) +
+					 matrix(centerRow-1)(centerCol-1) +
+					 matrix(centerRow-1)(centerCol+1) +
+					 matrix(centerRow)(centerCol-1) +
+					 matrix(centerRow)(centerCol+1) +
+					 matrix(centerRow+1)(centerCol-1) +
+					 matrix(centerRow+1)(centerCol) +
+					 matrix(centerRow+1)(centerCol+1)
+				}else{
+					sum += matrix(centerRow-1)(centerCol)
+					sum += matrix(centerRow-1)(centerCol-1)
+					sum += matrix(centerRow)(centerCol-1)
+					sum += matrix(centerRow+1)(centerCol-1)
+					sum += matrix(centerRow+1)(centerCol)
+				}
+			}else{
+				sum += matrix(centerRow-1)(centerCol)
+				sum += matrix(centerRow-1)(centerCol+1)
+				sum += matrix(centerRow)(centerCol+1)
+				sum += matrix(centerRow+1)(centerCol)
+				sum += matrix(centerRow+1)(centerCol+1)
+				sum += matrix(centerRow-1)(centerCol)
+				sum += matrix(centerRow+1)(centerCol)
+			}
+		}else{
+			if(centerCol >= 1){
+				if(centerCol < arrsz-1){
+					sum += matrix(centerRow-1)(centerCol)
+					sum += matrix(centerRow-1)(centerCol-1)
+					sum += matrix(centerRow-1)(centerCol+1)
+					sum += matrix(centerRow)(centerCol-1)
+					sum += matrix(centerRow)(centerCol+1)
+				}else{
+					sum += matrix(centerRow-1)(centerCol)
+					sum += matrix(centerRow-1)(centerCol-1)
+					sum += matrix(centerRow)(centerCol-1)
+				}
+			}else{
+				sum += matrix(centerRow-1)(centerCol)
+				sum += matrix(centerRow-1)(centerCol+1)
+				sum += matrix(centerRow)(centerCol+1)
+				sum += matrix(centerRow-1)(centerCol)	
+			}
+		}
+	}else{
+		if(centerRow < arrsz-1){
+			println("here")
+		}
+	}
 
+	return sum
+}
 def FindIndex(){
 	breakable{
 		for(i <- 0 to arrsz-1){
@@ -61,47 +91,59 @@ def FindIndex(){
 }
 
 def BuildMatrix(){
-	ArrInit()
 	//traverse initialize to 2
 	var traverse = 2
 
 	var row = arrsz/2
 	var col = arrsz/2
-	var curNum = 1
-	matrix(row)(col) = curNum
+	matrix(row)(col) = 1
+	PrintMat()
 	curNum += 1
-
-	while(row < arrsz && col < arrsz){
-		//start right 1
-		col += 1
-		//go up traverse-1
-		for(i <- 0 to traverse-1){
-			matrix(row-i)(col) = curNum
-			curNum += 1
+	breakable{
+		while(row < arrsz && col < arrsz){
+			//start right 1
+			col += 1
+			//go up traverse-1
+			for(i <- 0 to traverse-1){
+				curNum = GetSum(row-i,col)
+				matrix(row-i)(col) = curNum
+				if(curNum >= TARG)
+					break
+			}
+			
+			row = row - traverse + 1
+			//go left traverse
+			for(i <- 1 to traverse){
+				curNum = GetSum(row,col-i)
+				matrix(row)(col-i) = curNum
+				if(curNum >= TARG)
+					break		
+			}
+			
+			col = col - traverse
+			//traverse down
+			for(i <- 1 to traverse){
+				curNum = GetSum(row+i,col)
+				matrix(row+i)(col) = curNum
+				if(curNum >= TARG)
+					break
+			}
+			
+			row = row + traverse
+			//traverse right
+			for(i <- 1 to traverse){
+				curNum = GetSum(row,col+i)
+				matrix(row)(col+i) = curNum
+				if(curNum >= TARG)
+					break
+			}
+				
+			col = col + traverse
+				
+			traverse += 2
 		}
-		row = row - traverse + 1
-		//go left traverse
-		for(i <- 1 to traverse){
-			matrix(row)(col-i) = curNum
-			curNum += 1
-		}
-		col = col - traverse
-		//traverse down
-		for(i <- 1 to traverse){
-			matrix(row+i)(col) = curNum
-			curNum += 1
-		}
-		row = row + traverse
-		//traverse right
-		for(i <- 1 to traverse){
-			matrix(row)(col+i) = curNum
-			curNum += 1
-		}
-		col = col + traverse
-		if(curNum > TARG)
-			row = arrsz
-		traverse += 2
 	}
+	println("The first number is " + curNum)
 }
 def ArrInit(){
 	for(i <- 0 to arrsz-1)
@@ -114,10 +156,14 @@ def PrintMat(){
 		for(j <- 0 to arrsz-1){
 			var space = ""
 			if (matrix(i)(j) < 10)
-				space = "    "
+				space = "      "
 			else if(matrix(i)(j) < 100)
-				space = "   "
+				space = "     "
 			else if(matrix(i)(j) < 1000)
+				space = "    "
+			else if(matrix(i)(j) < 10000)
+				space = "   "
+			else if(matrix(i)(j) < 100000)
 				space = "  "
 			else
 				space = " "
